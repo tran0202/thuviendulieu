ec462bcc4f4508e49c9a5c9252a6e9c4207c8bec
Minhtho Tran <tran0202@yahoo.com>
2018-03-17 15:09:11 -0500

    Format

--- public_html/zyro/1.php
+++ public_html/zyro/1.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>TVDL - Home</title>
 	<base href="{{base_url}}" />
-			<meta name="viewport" content="width=1200" />
+		<meta name="viewport" content="width=1200" />
 		<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<!-- Facebook Open Graph -->
@@ -24,7 +24,8 @@
 	<link href="css/site.css?v=20180308163726" rel="stylesheet" type="text/css" />
 	<link href="css/common.css?ts=1520882525" rel="stylesheet" type="text/css" />
 	<link href="css/1.css?ts=1520882525" rel="stylesheet" type="text/css" />
-	{{ga_code}}<link rel="shortcut icon" href="/gallery/tvdl_favicon-ts1520881176.png" type="image/png" />
+	{{ga_code}}
+	<link rel="shortcut icon" href="/gallery/tvdl_favicon-ts1520881176.png" type="image/png" />
 	<script type="text/javascript">var currLang = '';</script>	
 	<!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
@@ -32,49 +33,103 @@
 </head>
 
 
-<body><div class="root"><div class="vbox wb_container" id="wb_header">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance0" class="wb_element wb-menu"><ul class="hmenu"><li class="active"><a href="" target="_self">Home</a></li><li><a href="Russia2018/" target="_self">Russia 2018</a></li><li><a href="NFL/" target="_self">NFL</a></li><li><a href="2017-US-Open-Mens-Singles/" target="_self">2017 US Open Men's Singles</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance1" class="wb_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle"><span class="wb_tr_ok">We Call It Soccer</span></h4></div><div id="wb_element_instance2" class="wb_element wb_element_picture"><a href=""><img alt="gallery/football" src="gallery_gen/8cd979f289c541d79d7ea74cf4b31f3e.png"></a></div><div id="wb_element_instance4" class="wb_element wb_element_picture"><img alt="gallery/ball_1" src="gallery_gen/740438132c8336073d625b0136159e8e_300x310.png"></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_main">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance5" class="wb_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;"><span class="wb_tr_ok">More than a game</span></h5></div><div id="wb_element_instance6" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><span class="wb-stl-highlight"><span class="wb_tr_ok">Soccer</span></span></p>
-<p class="wb-stl-normal"> </p></div><div id="wb_element_instance7" class="wb_element" style="width:470px;"><a class="wb_button" href="Russia2018/" style="display:inline-block;"><span>FIFA World Cup Russia 2018</span></a><a class="wb_button" href="NFL/" style="display:inline-block;"><span>NFL</span></a><a class="wb_button" href="2017-US-Open-Mens-Singles/" style="display:inline-block;"><span>2017 US Open Men's Singles</span></a></div><div id="wb_element_instance8" class="wb_element" style="width: 100%;">
-			<?php
-				global $show_comments;
-				if (isset($show_comments) && $show_comments) {
-					renderComments(1);
-			?>
-			<script type="text/javascript">
-				$(function() {
-					var block = $("#wb_element_instance8");
-					var comments = block.children(".wb_comments").eq(0);
-					var contentBlock = $("#wb_main");
-					contentBlock.height(contentBlock.height() + comments.height());
-				});
-			</script>
-			<?php
-				} else {
-			?>
-			<script type="text/javascript">
-				$(function() {
-					$("#wb_element_instance8").hide();
-				});
-			</script>
-			<?php
-				}
-			?>
-			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_footer">
-	
-<div class="wb_cont_inner" style="height: 130px;"><div id="wb_element_instance3" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p></div><div id="wb_element_instance9" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
-			$(function() {
-				var footer = $(".wb_footer");
-				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
-				if (!html) {
-					footer.parent().remove();
-					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
-					footer.css({height: ""});
-				}
-			});
-			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+<body>
+	<div class="root">
+		<div class="vbox wb_container" id="wb_header">
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance0" class="wb_element wb-menu">
+					<ul class="hmenu">
+						<li class="active"><a href="" target="_self">Home</a></li>
+						<li><a href="Russia2018/" target="_self">Russia 2018</a></li>
+						<li><a href="NFL/" target="_self">NFL</a></li>
+						<li><a href="2017-US-Open-Mens-Singles/" target="_self">2017 US Open Men's Singles</a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div id="wb_element_instance1" class="wb_element" style=" line-height: normal;">
+					<h4 class="wb-stl-pagetitle"><span class="wb_tr_ok">We Call It Soccer</span></h4>
+				</div>
+				<div id="wb_element_instance2" class="wb_element wb_element_picture">
+					<a href=""><img alt="gallery/football" src="gallery_gen/8cd979f289c541d79d7ea74cf4b31f3e.png"></a>
+				</div>
+				<div id="wb_element_instance4" class="wb_element wb_element_picture">
+					<img alt="gallery/ball_1" src="gallery_gen/740438132c8336073d625b0136159e8e_300x310.png">
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_main">
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance5" class="wb_element" style=" line-height: normal;">
+					<h5 class="wb-stl-subtitle" style="text-align: center;"><span class="wb_tr_ok">More than a game</span></h5>
+				</div>
+				<div id="wb_element_instance6" class="wb_element" style=" line-height: normal;">
+					<p class="wb-stl-normal" style="text-align: center;">
+						<span class="wb-stl-highlight"><span class="wb_tr_ok">Soccer</span></span>
+					</p>
+					<p class="wb-stl-normal"> </p>
+				</div>
+				<div id="wb_element_instance7" class="wb_element" style="width:470px;">
+					<a class="wb_button" href="Russia2018/" style="display:inline-block;"><span>FIFA World Cup Russia 2018</span></a>
+					<a class="wb_button" href="NFL/" style="display:inline-block;"><span>NFL</span></a>
+					<a class="wb_button" href="2017-US-Open-Mens-Singles/" style="display:inline-block;"><span>2017 US Open Men's Singles</span></a>
+				</div>
+				<div id="wb_element_instance8" class="wb_element" style="width: 100%;">
+					<?php
+						global $show_comments;
+						if (isset($show_comments) && $show_comments) {
+							renderComments(1);
+					?>
+					<script type="text/javascript">
+						$(function() {
+							var block = $("#wb_element_instance8");
+							var comments = block.children(".wb_comments").eq(0);
+							var contentBlock = $("#wb_main");
+							contentBlock.height(contentBlock.height() + comments.height());
+						});
+					</script>
+					<?php
+						} else {
+					?>
+					<script type="text/javascript">
+						$(function() {
+							$("#wb_element_instance8").hide();
+						});
+					</script>
+					<?php
+						}
+					?>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_footer">
+			<div class="wb_cont_inner" style="height: 130px;">
+				<div id="wb_element_instance3" class="wb_element" style=" line-height: normal;">
+					<p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p>
+				</div>
+				<div id="wb_element_instance9" class="wb_element" style="text-align: center; width: 100%;">
+					<div class="wb_footer"></div>
+					<script type="text/javascript">
+						$(function() {
+							var footer = $(".wb_footer");
+							var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+							if (!html) {
+								footer.parent().remove();
+								footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+								footer.css({height: ""});
+							}
+						});
+						</script>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="wb_sbg"></div>
+	</div>
+	{{hr_out}}
+</body>
 </html>
--- public_html/zyro/4.php
+++ public_html/zyro/4.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>TVDL - 2017 US Open Men's Singles</title>
 	<base href="{{base_url}}" />
-			<meta name="viewport" content="width=1200" />
+		<meta name="viewport" content="width=1200" />
 		<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<!-- Facebook Open Graph -->
@@ -24,7 +24,8 @@
 	<link href="css/site.css?v=20180308163726" rel="stylesheet" type="text/css" />
 	<link href="css/common.css?ts=1520882525" rel="stylesheet" type="text/css" />
 	<link href="css/4.css?ts=1520882525" rel="stylesheet" type="text/css" />
-	{{ga_code}}<link rel="shortcut icon" href="/gallery/tvdl_favicon-ts1520881176.png" type="image/png" />
+	{{ga_code}}
+	<link rel="shortcut icon" href="/gallery/tvdl_favicon-ts1520881176.png" type="image/png" />
 	<script type="text/javascript">var currLang = '';</script>	
 	<!--[if lt IE 9]>
 	<script src="js/html5shiv.min.js"></script>
@@ -32,77 +33,124 @@
 </head>
 
 
-<body><div class="root"><div class="vbox wb_container" id="wb_header">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance33" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">Home</a></li><li><a href="Russia2018/" target="_self">Russia 2018</a></li><li><a href="NFL/" target="_self">NFL</a></li><li class="active"><a href="2017-US-Open-Mens-Singles/" target="_self">2017 US Open Men's Singles</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance34" class="wb_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle"><span class="wb_tr_ok">We Call It Soccer</span></h4></div><div id="wb_element_instance35" class="wb_element wb_element_picture"><a href=""><img alt="gallery/football" src="gallery_gen/8cd979f289c541d79d7ea74cf4b31f3e.png"></a></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_main">
-	
-<div class="wb_cont_inner"><div id="wb_element_instance37" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1"><span style="color:#ffffff;"><span class="wb_tr_ok">New Content</span></span></h1>
-<?php
-	include_once('config.php');
-	$output = '';
-	$sql = 'SELECT t.name AS name, team_id, ' .
-	 	'group_id, g.name AS group_name, group_order, ' .
-	 	'parent_group_id, pg.name AS parent_group_name, parent_group_order, tt.tournament_id ' . 
-		'FROM team_tournament tt ' .
-		'LEFT JOIN team t ON t.id = tt.team_id ' .
-		'LEFT JOIN `group` g ON g.id = tt.group_id ' .
-		'LEFT JOIN `group` pg ON pg.id = tt.parent_group_id ' .
-		'WHERE tt.tournament_id = 4 ' .
-		'ORDER BY team_id';
-    $query = $connection -> prepare($sql);
-	$query -> execute();
-	$count = $query -> rowCount();
-	if ($count != 0) {
-		while ($row = $query -> fetch(PDO::FETCH_ASSOC)) {
-			$output .= ''.$row['group_name'].' '.$row['group_order'].' '.$row['name'].'<br>';
-		}
-	} 
-	else {
-		$output = '<h2>No result found!</h2>';
-	}
-?>
-<?php echo $output; ?>
-<p> </p>
+<body>
+	<div class="root">
+		<div class="vbox wb_container" id="wb_header">
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance33" class="wb_element wb-menu">
+					<ul class="hmenu">
+						<li><a href="" target="_self">Home</a></li>
+						<li><a href="Russia2018/" target="_self">Russia 2018</a></li>
+						<li><a href="NFL/" target="_self">NFL</a></li>
+						<li class="active"><a href="2017-US-Open-Mens-Singles/" target="_self">2017 US Open Men's Singles</a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div id="wb_element_instance34" class="wb_element" style=" line-height: normal;">
+					<h4 class="wb-stl-pagetitle"><span class="wb_tr_ok">We Call It Soccer</span></h4>
+				</div>
+				<div id="wb_element_instance35" class="wb_element wb_element_picture">
+					<a href=""><img alt="gallery/football" src="gallery_gen/8cd979f289c541d79d7ea74cf4b31f3e.png"></a>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_main">
+			<div class="wb_cont_inner">
+				<div id="wb_element_instance37" class="wb_element" style=" line-height: normal;">
+					<h1 class="wb-stl-heading1"><span style="color:#ffffff;"><span class="wb_tr_ok">2017 US Open Men's Singles</span></span></h1>
+					<?php
+						include_once('config.php');
+						$output = '';
+						$sql = 'SELECT t.name AS name, team_id, ' .
+						 	'group_id, g.name AS group_name, group_order, ' .
+						 	'parent_group_id, pg.name AS parent_group_name, parent_group_order, tt.tournament_id ' . 
+							'FROM team_tournament tt ' .
+							'LEFT JOIN team t ON t.id = tt.team_id ' .
+							'LEFT JOIN `group` g ON g.id = tt.group_id ' .
+							'LEFT JOIN `group` pg ON pg.id = tt.parent_group_id ' .
+							'WHERE tt.tournament_id = 4 ' .
+							'ORDER BY team_id';
+					    $query = $connection -> prepare($sql);
+						$query -> execute();
+						$count = $query -> rowCount();
+						if ($count != 0) {
+							while ($row = $query -> fetch(PDO::FETCH_ASSOC)) {
+								$output .= ''.$row['group_name'].' '.$row['group_order'].' '.$row['name'].'<br>';
+							}
+						} 
+						else {
+							$output = '<h2>No result found!</h2>';
+						}
+					?>
+					<?php echo $output; ?>
+					<p> </p>
 
-<!--<p class="wb-stl-normal"><span style="color:#ffffff;">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us...</span><a href="Teams/"><span style="color:#ffffff;">Please fill the contact form on our...</span></a><span style="color:#ffffff;">Website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the...</span></p>-->
-</div><div id="wb_element_instance38" class="wb_element wb_element_picture"><!--<img alt="gallery/rush-1335365_1280" src="gallery_gen/deaceea944c1e5c8faa97dbc938638a2_330x230.jpg">--></div><div id="wb_element_instance39" class="wb_element wb_element_picture"><!--<img alt="gallery/soccer-933037_1280" src="gallery_gen/9f1f8dd4b573022ccb5f58534e63c223_340x230.jpg">--></div><div id="wb_element_instance40" class="wb_element wb_element_picture"><!--<img alt="gallery/football-452569_640" src="gallery_gen/02aca76cc69c5b3f26f749b67d453d64_340x230.jpg">--></div><div id="wb_element_instance41" class="wb_element" style=" line-height: normal;"><!--<p class="wb-stl-normal"><span style="color:#ffffff;">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form...</span></p>--></div><div id="wb_element_instance42" class="wb_element" style=" line-height: normal;"><!--<p class="wb-stl-normal"><span style="color:#ffffff;">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best...</span></p>--></div><div id="wb_element_instance43" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance44" class="wb_element" style="width: 100%;">
-			<?php
-				global $show_comments;
-				if (isset($show_comments) && $show_comments) {
-					renderComments(4);
-			?>
-			<script type="text/javascript">
-				$(function() {
-					var block = $("#wb_element_instance44");
-					var comments = block.children(".wb_comments").eq(0);
-					var contentBlock = $("#wb_main");
-					contentBlock.height(contentBlock.height() + comments.height());
-				});
-			</script>
-			<?php
-				} else {
-			?>
-			<script type="text/javascript">
-				$(function() {
-					$("#wb_element_instance44").hide();
-				});
-			</script>
-			<?php
-				}
-			?>
-			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
-<div class="vbox wb_container" id="wb_footer">
-	
-<div class="wb_cont_inner" style="height: 130px;"><div id="wb_element_instance36" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p></div><div id="wb_element_instance45" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
-			$(function() {
-				var footer = $(".wb_footer");
-				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
-				if (!html) {
-					footer.parent().remove();
-					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
-					footer.css({height: ""});
-				}
-			});
-			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+				</div>
+				<div id="wb_element_instance38" class="wb_element wb_element_picture"><!--<img alt="gallery/rush-1335365_1280" src="gallery_gen/deaceea944c1e5c8faa97dbc938638a2_330x230.jpg">--></div>
+				<div id="wb_element_instance39" class="wb_element wb_element_picture"><!--<img alt="gallery/soccer-933037_1280" src="gallery_gen/9f1f8dd4b573022ccb5f58534e63c223_340x230.jpg">--></div>
+				<div id="wb_element_instance40" class="wb_element wb_element_picture"><!--<img alt="gallery/football-452569_640" src="gallery_gen/02aca76cc69c5b3f26f749b67d453d64_340x230.jpg">--></div>
+				<div id="wb_element_instance41" class="wb_element" style=" line-height: normal;"><!--<p class="wb-stl-normal"><span style="color:#ffffff;">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form...</span></p>--></div>
+				<div id="wb_element_instance42" class="wb_element" style=" line-height: normal;"><!--<p class="wb-stl-normal"><span style="color:#ffffff;">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best...</span></p>--></div>
+				<div id="wb_element_instance43" class="wb_element wb_element_shape">
+					<div class="wb_shp"></div>
+				</div>
+				<div id="wb_element_instance44" class="wb_element" style="width: 100%;">
+					<?php
+						global $show_comments;
+						if (isset($show_comments) && $show_comments) {
+							renderComments(4);
+					?>
+					<script type="text/javascript">
+						$(function() {
+							var block = $("#wb_element_instance44");
+							var comments = block.children(".wb_comments").eq(0);
+							var contentBlock = $("#wb_main");
+							contentBlock.height(contentBlock.height() + comments.height());
+						});
+					</script>
+					<?php
+						} else {
+					?>
+					<script type="text/javascript">
+						$(function() {
+							$("#wb_element_instance44").hide();
+						});
+					</script>
+					<?php
+						}
+					?>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="vbox wb_container" id="wb_footer">
+			<div class="wb_cont_inner" style="height: 130px;">
+				<div id="wb_element_instance36" class="wb_element" style=" line-height: normal;">
+					<p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p>
+				</div>
+				<div id="wb_element_instance45" class="wb_element" style="text-align: center; width: 100%;">
+					<div class="wb_footer"></div>
+					<script type="text/javascript">
+					$(function() {
+						var footer = $(".wb_footer");
+						var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+						if (!html) {
+							footer.parent().remove();
+							footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+							footer.css({height: ""});
+						}
+					});
+					</script>
+				</div>
+			</div>
+			<div class="wb_cont_outer"></div>
+			<div class="wb_cont_bg"></div>
+		</div>
+		<div class="wb_sbg"></div>
+	</div>
+	{{hr_out}}
+</body>
 </html>
