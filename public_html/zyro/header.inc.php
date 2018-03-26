f1a86d7769b4499a4ed75094f1e1992190cf105d
Minhtho Tran <tran0202@yahoo.com>
2018-03-26 06:28:47 -0500

    Header

--- public_html/zyro/header.inc.php
+++ public_html/zyro/header.inc.php
@@ -1,4 +1,31 @@
 
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<base href="{{base_url}}" />
+<meta name="viewport" content="width=1200" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<!-- Facebook Open Graph -->
+<meta name="og:title" content="Home" />
+<meta name="og:description" content="" />
+<meta name="og:image" content="" />
+<meta name="og:type" content="article" />
+<meta name="og:url" content="{{curr_url}}" />
+<!-- Facebook Open Graph end -->
+
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/main.js?v=20180222164022" type="text/javascript"></script>
+
+<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+<link href="css/site.css?v=20180308163726" rel="stylesheet" type="text/css" />
+<link href="css/common.css?ts=1520882525" rel="stylesheet" type="text/css" />
+{{ga_code}}
+<link rel="shortcut icon" href="/gallery/tvdl_favicon-ts1520881176.png" type="image/png" />
+<script type="text/javascript">var currLang = '';</script>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.min.js"></script>
+<![endif]-->
 <link href="https://fonts.googleapis.com/css?family=Courgette|Merriweather|Monoton|Oswald|Roboto" rel="stylesheet">
 <script src="js/sticky_header.js" type="text/javascript"></script>
 <script src="js/modal_position.js" type="text/javascript"></script>
