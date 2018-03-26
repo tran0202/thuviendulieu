e5acc877feeafd46932693b919a92c5c3792f342
Minhtho Tran <tran0202@yahoo.com>
2018-03-26 05:46:17 -0500

    Footer

---
+++ public_html/zyro/footer.php
@@ -0,0 +1,25 @@
+
+<div class="vbox wb_container" id="wb_footer">
+    <div class="wb_cont_inner" style="height: 130px;">
+        <div id="wb_element_instance3" class="wb_element" style=" line-height: normal;">
+            <p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p>
+        </div>
+        <div id="wb_element_instance9" class="wb_element" style="text-align: center; width: 100%;">
+            <div class="wb_footer"></div>
+            <script type="text/javascript">
+                $(function() {
+                    var footer = $(".wb_footer");
+                    var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+                    if (!html) {
+                        footer.parent().remove();
+                        footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+                        footer.css({height: ""});
+                    }
+                });
+            </script>
+        </div>
+    </div>
+    <div class="wb_cont_outer"></div>
+    <div class="wb_cont_bg"></div>
+</div>
+<div class="wb_sbg"></div>
--- public_html/zyro/soccer_schedule.php
+++ public_html/zyro/soccer_schedule.php
@@ -156,30 +156,7 @@
 			<div class="wb_cont_outer"></div>
 			<div class="wb_cont_bg"></div>
 		</div>
-		<div class="vbox wb_container" id="wb_footer">
-			<div class="wb_cont_inner" style="height: 130px;">
-				<div id="wb_element_instance13" class="wb_element" style=" line-height: normal;">
-					<p class="wb-stl-footer">© 2018 <a href="http://thuviendulieu.000webhostapp.com">thuviendulieu.000webhostapp.com</a></p>
-				</div>
-				<div id="wb_element_instance19" class="wb_element" style="text-align: center; width: 100%;">
-					<div class="wb_footer"></div>
-					<script type="text/javascript">
-						$(function() {
-							var footer = $(".wb_footer");
-							var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
-							if (!html) {
-								footer.parent().remove();
-								footer = $("#wb_footer, #wb_footer .wb_cont_inner");
-								footer.css({height: ""});
-							}
-						});
-					</script>
-				</div>
-			</div>
-			<div class="wb_cont_outer"></div>
-			<div class="wb_cont_bg"></div>
-		</div>
-		<div class="wb_sbg"></div>
+        <?php include_once('footer.php'); ?>
 	</div>
 	{{hr_out}}
 </body>
