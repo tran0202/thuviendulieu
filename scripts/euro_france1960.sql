fde85a082ad245051d9e01056e08432fcb88536f
Minhtho Tran <tran0202@yahoo.com>
2018-10-06 22:33:50 -0500

    Euro 1960 France

---
+++ scripts/euro_france1960.sql
@@ -0,0 +1,140 @@
+# Insert new tournament
+# Replace all the tournament_id
+
+# Euro 1960 France
+
+INSERT INTO tournament (name, start_date, end_date, tournament_type_id, logo_filename, golden_goal_rule, points_for_win)
+VALUES ('UEFA Euro 1960 France', '1960-07-06', '1960-07-10', 11, 'Euro_1960.png', null, 2);
+
+INSERT INTO group_tournament (tournament_id, group_id, group_order, parent_group_id, parent_group_order)
+VALUES (82, 44, 1, 40, 1),
+	   (82, 45, 2, 40, 2),
+	   (82, 46, 3, 40, 3);
+
+SELECT * FROM `team` WHERE team_type_id = 1 ORDER BY name;
+
+# INSERT INTO team (name, team_type_id, nation_id, parent_team_id)
+# VALUES ('CIS', 1, 228, 23);
+
+INSERT INTO team_tournament (team_id, tournament_id, group_id, group_order)
+VALUES (21, 82, 1, 1),
+	   (224, 82, 1, 2),
+	   (230, 82, 1, 3),
+	   (228, 82, 1, 4);
+
+INSERT INTO `match` (home_team_id, home_team_score, home_team_extra_time_score, home_team_penalty_score,
+					 away_team_id, away_team_score, away_team_extra_time_score, away_team_penalty_score,
+					 tournament_id, match_date, match_time, match_order, round_id, stage_id, bracket_order)
+VALUES (21, 4, null, null, 224, 5, null, null, 82, '1960-07-06', '200000', 1, 44, 40, 2),
+	   (230, 0, null, null, 228, 3, null, null, 82, '1960-07-06', '213000', 2, 44, 40, 1),
+	   (230, 2, null, null, 21, 0, null, null, 82, '1960-07-09', '213000', 3, 45, 40, 4),
+	   (228, 1, 1, null, 224, 1, 0, null, 82, '1960-07-10', '213000', 4, 46, 40, 3);
+
+CREATE TABLE IF NOT EXISTS tournament (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL UNIQUE,
+	start_date DATE,
+	end_date DATE,
+	logo_filename VARCHAR(255),
+	tournament_type_id INT,
+	parent_tournament_id INT,
+	points_for_win TINYINT UNSIGNED,
+	golden_goal_rule TINYINT UNSIGNED,
+	FOREIGN KEY (tournament_type_id) REFERENCES tournament_type(id),
+	FOREIGN KEY (parent_tournament_id) REFERENCES tournament(id)
+);
+
+CREATE TABLE IF NOT EXISTS team (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL UNIQUE,
+	team_type_id INT,
+	nation_id INT,
+	parent_team_id INT,
+	FOREIGN KEY (team_type_id) REFERENCES team_type(id),
+	FOREIGN KEY (nation_id) REFERENCES nation(id),
+	FOREIGN KEY (parent_team_id) REFERENCES team(id),
+	code VARCHAR(255),
+	short_name VARCHAR(255),
+	official_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS team_tournament (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	team_id INT NOT NULL,
+	tournament_id INT NOT NULL,
+	group_id INT,
+	group_order TINYINT UNSIGNED,
+	parent_group_id INT,
+	parent_group_order TINYINT UNSIGNED,
+	seed TINYINT UNSIGNED,
+	FOREIGN KEY (team_id) REFERENCES team(id),
+	FOREIGN KEY (tournament_id) REFERENCES tournament(id),
+	FOREIGN KEY (group_id) REFERENCES `group`(id),
+	FOREIGN KEY (parent_group_id) REFERENCES `group`(id)
+);
+
+CREATE TABLE IF NOT EXISTS `match` (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	home_team_id INT,
+	home_team_score TINYINT UNSIGNED,
+	away_team_id INT,
+	away_team_score TINYINT UNSIGNED,
+	home_team_first_leg_score TINYINT UNSIGNED,
+	away_team_first_leg_score TINYINT UNSIGNED,
+	tournament_id INT NOT NULL,
+	match_date DATE,
+	match_time TIME,
+	match_order SMALLINT UNSIGNED,
+	bracket_order TINYINT UNSIGNED,
+	waiting_home_team VARCHAR(255),
+	waiting_away_team VARCHAR(255),
+	home_team_extra_time_score TINYINT UNSIGNED,
+	away_team_extra_time_score TINYINT UNSIGNED,
+	home_team_penalty_score TINYINT UNSIGNED,
+	away_team_penalty_score TINYINT UNSIGNED,
+	round_id INT,
+	round_order TINYINT UNSIGNED,
+	stage_id INT,
+	stage_order TINYINT UNSIGNED,
+	group_id INT,
+	home_retired TINYINT UNSIGNED,
+	away_retired TINYINT UNSIGNED,
+	home_set1_score TINYINT UNSIGNED,
+	away_set1_score TINYINT UNSIGNED,
+	home_set1_tiebreak TINYINT UNSIGNED,
+	away_set1_tiebreak TINYINT UNSIGNED,
+	home_set2_score TINYINT UNSIGNED,
+	away_set2_score TINYINT UNSIGNED,
+	home_set2_tiebreak TINYINT UNSIGNED,
+	away_set2_tiebreak TINYINT UNSIGNED,
+	home_set3_score TINYINT UNSIGNED,
+	away_set3_score TINYINT UNSIGNED,
+	home_set3_tiebreak TINYINT UNSIGNED,
+	away_set3_tiebreak TINYINT UNSIGNED,
+	home_set4_score TINYINT UNSIGNED,
+	away_set4_score TINYINT UNSIGNED,
+	home_set4_tiebreak TINYINT UNSIGNED,
+	away_set4_tiebreak TINYINT UNSIGNED,
+	home_set5_score TINYINT UNSIGNED,
+	away_set5_score TINYINT UNSIGNED,
+	home_set5_tiebreak TINYINT UNSIGNED,
+	away_set5_tiebreak TINYINT UNSIGNED,
+	FOREIGN KEY (home_team_id) REFERENCES team(id),
+	FOREIGN KEY (away_team_id) REFERENCES team(id),
+	FOREIGN KEY (tournament_id) REFERENCES tournament(id),
+	FOREIGN KEY (round_id) REFERENCES `group`(id),
+	FOREIGN KEY (stage_id) REFERENCES `group`(id),
+	FOREIGN KEY (group_id) REFERENCES `group`(id)
+);
+
+CREATE TABLE IF NOT EXISTS group_tournament (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	tournament_id INT NOT NULL,
+	group_id INT,
+	group_order TINYINT UNSIGNED,
+	parent_group_id INT,
+	parent_group_order TINYINT UNSIGNED,
+	FOREIGN KEY (tournament_id) REFERENCES tournament(id),
+	FOREIGN KEY (group_id) REFERENCES `group`(id),
+	FOREIGN KEY (parent_group_id) REFERENCES `group`(id)
+);
